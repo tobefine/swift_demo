7c7518087c796895ba30c73d2eb5bb1f0c131e99
xda <xda@FCP.local>
2017-07-18 20:50:46 +0800

    init

--- swift/build/Pods.build/Debug-iphonesimulator/Pods-swift.build/DerivedSources/Pods_swift_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char Pods_swiftVersionString[];
- extern const double Pods_swiftVersionNumber;
-
- const unsigned char Pods_swiftVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_swift  PROJECT:Pods-1" "\n";
- const double Pods_swiftVersionNumber __attribute__ ((used)) = (double)1.;
--- swift/build/swift.build/Debug-iphonesimulator/swift.build/Script-98B68A1258E561E47B67FE51.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"${SRCROOT}/Pods/Target Support Files/Pods-swift/Pods-swift-frameworks.sh"
-
